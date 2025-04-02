2e5118cf352f310519648f729c33ea352615541f
Carine <carinerandrianarijaona@gmail.com>
2025-04-02 14:53:46 +0200

    autres commandes

--- exo1.js
+++ exo1.js
@@ -12,3 +12,20 @@ Affiche le nombre de livres listés dans la table
 5. SELECT bookname from books where author="Simone de Beauvoir" AND parution_date > 1950;
 6. SELECT COUNT(*) FROM books;
 */
+
+/* Commandes:
+INSERT INTO books (bookname, author, parution_date)
+VALUES
+('Le Deuxième Sexe', 'Simone de Beauvoir', 1949),
+('Une chambre à soi', 'Virginia Woolf', 1929),
+('Le rire de la Méduse', 'Hélène Cixous', 1975),
+('La cloche de détresse', 'Sylvia Plath', 1963),
+('Sorcières : La puissance invaincue des femmes', 'Mona Chollet', 2018),
+('Les Argonautes', ' Maggie Nelson', 2015),
+('Nous sommes tous des féministes', 'Chimamanda Ngozi Adichie', 2014),
+('L''Amie prodigieuse', 'Elena Ferrante', 2011),
+('Futur-es', 'Lauren Bastide', 2022),
+('King Kong Théorie', 'Virginie Despentes', 2006),
+('La Femme rompue', 'Simone de Beauvoir', 1967),
+('La Vieillesse', 'Simone de Beauvoir', 1970);
+*/
